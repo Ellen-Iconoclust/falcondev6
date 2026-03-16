3e314e2f4c19993f6a95d623cbde31c911410c8f
Ellen-Iconoclust <elleniconoclust@gmail.com>
2026-03-16 14:20:59 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,21 @@ import React from 'react';
 
 export type Page = 'FILE' | 'EDIT' | 'SEARCH' | 'RUN' | 'COMPILE' | 'DEBUG' | 'PROJECT' | 'OPTIONS' | 'WINDOW' | 'HELP';
 
+export type FileType = string | null;
+
+export interface TerminalState {
+  isRunning: boolean;
+  file: FileType;
+  step: 'NAME' | 'EMAIL' | 'MESSAGE' | 'SUBMITTING' | 'DONE' | null;
+  input: string;
+  formData: {
+    name: string;
+    email: string;
+    message: string;
+  };
+  output: string[];
+}
+
 export interface Project {
   id: string;
   title: string;
